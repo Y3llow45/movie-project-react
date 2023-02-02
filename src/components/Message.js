1ddb4aa241dd26c499a895efa1a355d0a1ed6916
Yellow <bofengejn@gmail.com>
2023-02-02 21:51:28 +0200

    Update Message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -5,7 +5,12 @@ class Message extends Component {
         super(props)
     }
     componentDidMount() {
-        
+        setTimeout(() => {
+            this.forceUpdate();
+        }, 1000);
+    }
+    componentDidUpdate() {
+        console.log('component did update');
     }
     render() {
         return <span>{this.props.text}</span>
