715e16e33caa0917397b73c3f02f9da0be26c13e
Yellow <bofengejn@gmail.com>
2023-02-06 21:28:54 +0200

    Update refreshComponent.js

--- src/components/hoc/refreshComponent.js
+++ src/components/hoc/refreshComponent.js
@@ -1,21 +1,22 @@
-import { Component } from "react";
+import { Component } from 'react';
 
-function refreshComponent(WrappedComponent, time=2500) {
+function refreshComponent(WrappedComponent, time = 2500) {
     return class extends Component {
         constructor(props) {
-            super(props)
+            super(props);
             this.state = {
                 refreshCount: 0
             }
         }
         componentDidMount() {
             setTimeout(() => {
-                this.setState({refreshComponent: this.state.refreshCount + 1})
-            }, time)
+                this.setState({refreshCount: this.state.refreshCount + 1})
+            }, time);
         }
         render() {
-            return <WrappedComponent {...this.props} refreshCount={this.state.refreshCount}></WrappedComponent>
+            return <WrappedComponent {...this.props} refreshCount={this.state.refreshCount} />;
         }
     }
 }
+
 export default refreshComponent;
